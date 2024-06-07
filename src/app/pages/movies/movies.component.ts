1cd440b79fc2da0526b154a716309109acd70314
Tobiloba <tobitobe66@gmail.com>
2024-06-07 06:33:28 +0100

    fix: Resolve Image Error

---
+++ src/app/pages/movies/movies.component.ts
@@ -0,0 +1,36 @@
+import { Component, DestroyRef, inject } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+import { AsyncPipe } from '@angular/common';
+import { Movie } from '../../models/movie';
+import { MovieComponent } from '../../components/movie/movie.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-movies',
+  standalone: true,
+  imports: [AsyncPipe, MovieComponent, InfiniteScrollModule],
+  templateUrl: './movies.component.html',
+  styleUrl: './movies.component.css',
+})
+export class MoviesComponent {
+  private  moviesService  =  inject(MoviesService);
+  private  pageNumber  =  1;
+  private destroyRef  =  inject(DestroyRef)
+  public  moviesObs$  =  this.moviesService.fetchMoviesByType('popular', this.pageNumber);
+  public  moviesResults:  Movie[] = [];
+
+  onScroll():  void {
+    this.pageNumber++;
+    this.moviesObs$  =  this.moviesService.fetchMoviesByType('popular', this.pageNumber);
+    this.moviesObs$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((data) => {
+      this.moviesResults  =  this.moviesResults.concat(data.results);
+    });
+  }
+
+  ngOnInit(){
+    this.moviesObs$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((data) => {
+      this.moviesResults  =  data.results;
+    });
+  }
+}
